762fbcaad2a2cd40aa7d11b2cd887f9006b72010
Théo Knoepflin <theo.knoepflin@gmail.com>
2021-02-03 17:54:31 +0100

    Mise à jour de la structure de la base de données pour import initial

--- backend/groupomania-structure-db.sql
+++ backend/groupomania-structure-db.sql
@@ -2,7 +2,8 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Généré le :  ven. 29 jan. 2021 à 18:22
+-- Hôte : 127.0.0.1
+-- Généré le :  mer. 03 fév. 2021 à 17:52
 -- Version du serveur :  8.0.22
 -- Version de PHP :  7.3.2
 
@@ -35,7 +36,6 @@ CREATE TABLE `comments` (
   `comment_author_id` int UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 -- --------------------------------------------------------
 
 --
@@ -67,6 +67,9 @@ CREATE TABLE `users` (
   `is_admin` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Index pour les tables déchargées
+--
 
 --
 -- Index pour la table `comments`
@@ -92,32 +95,38 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `id_UNIQUE` (`id`),
   ADD UNIQUE KEY `email_UNIQUE` (`email`);
 
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
 
 --
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
 
 --
 -- AUTO_INCREMENT pour la table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
+--
+-- Contraintes pour les tables déchargées
+--
 
 --
 -- Contraintes pour la table `comments`
 --
 ALTER TABLE `comments`
-  ADD CONSTRAINT `fk_comments_posts1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`),
-  ADD CONSTRAINT `fk_comments_users1` FOREIGN KEY (`comment_author_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `fk_comments_posts1` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_comments_users1` FOREIGN KEY (`comment_author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT;
 
 --
 -- Contraintes pour la table `posts`
