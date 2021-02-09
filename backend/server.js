08368015799ea40b8f20eeef8c38a0b91d7b08dd
Théo Knoepflin <theo.knoepflin@gmail.com>
2021-02-09 13:12:15 +0100

    Installation express-xss-sanitizer

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express"); 
 const bodyParser = require("body-parser"); 
 const helmet = require("helmet"); 
+const xss = require('express-xss-sanitizer'); 
 
 const path = require('path'); 
 /* const { nextTick } = require("process"); */
@@ -17,6 +18,7 @@ app.use((req, res, next) => { //Déclaration des headers CORS
 app.use(helmet()); 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(xss()); 
 
 app.get("/", (req, res) => {
 	res.json("Bienvenue sur l'API REST du réseau social de Groupomania"); 
